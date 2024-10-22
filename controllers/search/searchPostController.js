0b070f5c6183365dc5f5a800377371f2ac275273
divyam100 <redkardivyam@gmail.com>
2024-10-23 02:26:25 +0530

    testing

--- controllers/search/searchPostController.js
+++ controllers/search/searchPostController.js
@@ -2,7 +2,7 @@ const Post = require('../../models/postModel');  // Assuming the Post model is i
 
 exports.searchPosts = async (req, res) => {
   try {
-    const { searchTerm } = req.body; // Get search term from query parameter
+    const { searchTerm } = req.body; // Get search term from request body
 
     if (!searchTerm) {
       return res.status(400).json({ message: 'Search term is required' });
@@ -14,8 +14,9 @@ exports.searchPosts = async (req, res) => {
         { post_title: { $regex: searchTerm, $options: 'i' } },  // Case-insensitive search on post title
         { post_hashes: { $in: [searchTerm] } }  // Check if the search term exists in the post_hashes array
       ]
-    }).populate('user_id', 'username name pfp_link')  // Optionally populate the user who created the post
-        // Select the fields to return
+    })
+    .populate('user_id', 'username name pfp_link')  // Populate user details excluding password
+    .select('post_title post_content post_hashes post_image_link visibility post_points createdAt user_id');  // Select fields to return
 
     // If no posts are found, return a message
     if (posts.length === 0) {
@@ -30,4 +31,3 @@ exports.searchPosts = async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 };
-
